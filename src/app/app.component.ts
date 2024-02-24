dbf4fbc1a783c0712ef5b03adea065f843f52ee8
Mayuresh <mayureshngorantiwar@gmail.com>
2024-02-24 19:19:28 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-
 export class AppComponent {
   userprofilephoto =
     'https://images.unsplash.com/photo-1707697781368-d4f7a152510e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTh8fHxlbnwwfHx8fHw%3D';
@@ -19,49 +18,56 @@ export class AppComponent {
     lastname: 'Gorantiwar',
   };
 
-  ys1: { image_url: string; text: string } = {
-    image_url:
-      'https://images.unsplash.com/photo-1682686581413-0a0ec9bb35bb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNDZ8fHxlbnwwfHx8fHw%3D',
-    text: 'Swimming',
-  };
-
-  ys2: { image_url: string; text: string } = {
-    image_url:
-      'https://images.unsplash.com/photo-1708281789753-a414433ea272?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNTB8fHxlbnwwfHx8fHw%3D',
-    text: 'Star Gazing',
-  };
-
-  ys3: { image_url: string; text: string } = {
-    image_url:
-      'https://images.unsplash.com/photo-1707343843344-011332037abb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNTF8fHxlbnwwfHx8fHw%3D',
-    text: 'Beach Exploration',
-  };
-
-  ys4: { image_url: string; text: string } = {
-    image_url:
-      'https://images.unsplash.com/photo-1708246116930-98cdc77b46c9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNTR8fHxlbnwwfHx8fHw%3D',
-    text: 'Travelling',
-  };
+  shortcuts: Array<{ image_url: string; text: string }> = [
+    {
+      image_url:
+        'https://images.unsplash.com/photo-1682686581413-0a0ec9bb35bb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNDZ8fHxlbnwwfHx8fHw%3D',
+      text: 'Swimming',
+    },
+    {
+      image_url:
+        'https://images.unsplash.com/photo-1708281789753-a414433ea272?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNTB8fHxlbnwwfHx8fHw%3D',
+      text: 'Star Gazing',
+    },
+    {
+      image_url:
+        'https://images.unsplash.com/photo-1707343843344-011332037abb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNTF8fHxlbnwwfHx8fHw%3D',
+      text: 'Beach Exploration',
+    },
+    {
+      image_url:
+        'https://images.unsplash.com/photo-1708246116930-98cdc77b46c9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNTR8fHxlbnwwfHx8fHw%3D',
+      text: 'Travelling',
+    },
+  ];
 
   your_story =
     'https://images.unsplash.com/photo-1706550037742-0e6b5d786811?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNTd8fHxlbnwwfHx8fHw%3D';
 
-  story2: { url: string; name: string } = {
+  story2: { profile_url: string; url: string; name: string } = {
+    profile_url:
+      'https://plus.unsplash.com/premium_photo-1708194041762-245c26f3c8bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D',
     url: 'https://images.unsplash.com/photo-1682687220363-35e4621ed990?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyNTl8fHxlbnwwfHx8fHw%3D',
     name: 'NEOM',
   };
 
-  story3: { url: string; name: string } = {
+  story3: { profile_url: string; url: string; name: string } = {
+    profile_url:
+      'https://images.unsplash.com/photo-1477064601209-5ed2b9f3b1fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNHx8fGVufDB8fHx8fA%3D%3D',
     url: 'https://images.unsplash.com/photo-1708164863710-14fa216daa5c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNjJ8fHxlbnwwfHx8fHw%3D',
     name: 'DM DAVID',
   };
 
-  story4: { url: string; name: string } = {
+  story4: { profile_url: string; url: string; name: string } = {
+    profile_url:
+      'https://images.unsplash.com/photo-1683009427479-c7e36bbb7bca?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2MXx8fGVufDB8fHx8fA%3D%3D',
     url: 'https://images.unsplash.com/photo-1708074017412-be6a9e38e4ab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNjZ8fHxlbnwwfHx8fHw%3D',
     name: 'ASHFORD MARX',
   };
 
-  story5: { url: string; name: string } = {
+  story5: { profile_url: string; url: string; name: string } = {
+    profile_url:
+      'https://images.unsplash.com/photo-1707707289494-af5b6017f7ab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NHx8fGVufDB8fHx8fA%3D%3D',
     url: 'https://images.unsplash.com/photo-1708022799750-afbe4be26f73?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNzZ8fHxlbnwwfHx8fHw%3D',
     name: 'NICHOLAS',
   };
@@ -87,7 +93,16 @@ export class AppComponent {
     },
   ];
 
-  posts = [
+  posts: Array<{
+    imgSrc: string;
+    userName: string;
+    timestamp: string;
+    postText: string;
+    postImgSrc: string;
+    likeCount: number;
+    commentCount: number;
+    shareCount: number;
+  }> = [
     {
       imgSrc: this.userprofilephoto,
       userName: this.user.firstname + ' ' + this.user.lastname,
